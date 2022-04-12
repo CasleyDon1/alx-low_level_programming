67a529a3dd69c423247e2408295759b51b87a4f5
Casley <Casleynkosi@gmail.com>
2022-04-12 11:53:55 +0000

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+ * main - entry point
+ *
+ * Return: nothing, return void
+ */
+
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+
+return (0);
+
+}
