41a20ae91465c834f39fe0badf3b1a6f13947908
Casley <Casleynkosi@gmail.com>
2022-04-20 11:49:59 +0000

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int -swap the values of two integers a and b
+ * @a: A pointer to an int that will br updated
+ * @b: B pointer to an in that will be updated
+ * Return: void that means our answer is correct
+ */
+
+void swap_int(int a*, int *b)
+{
+int f;
+
+f = *a;
+*a = *b;
+*b = f;
+}
