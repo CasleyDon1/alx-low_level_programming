c80ddb5abddce70442ffccb4537559ddefd31cdc
Casley <Casleynkosi@gmail.com>
2022-04-25 14:59:57 +0000

    Task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -21,7 +21,7 @@ void print_diagsums(int *a, int size)
 	while (i < size) /* number repetitions */
 	{
 		/* sums for diagsums */
-		sum1 = sum1 + *(a + 1 * size + i);
+		sum1 = sum1 + *(a + i * size + i);
 		sum2 = sum2 + *(a + i * size - i - 1);
 
 		i++; /* add +1 */
