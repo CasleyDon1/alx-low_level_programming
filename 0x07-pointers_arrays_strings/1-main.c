acab69c3e334e3fe22a4905ff1a74fd3818563c6
Casley <Casleynkosi@gmail.com>
2022-04-25 11:27:29 +0000

    Task 1

--- 0x07-pointers_arrays_strings/1-main.c
+++ 0x07-pointers_arrays_strings/1-main.c
@@ -4,7 +4,7 @@
 /**
  * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
- * @size: the of the memory to print
+ * @size: the size of the memory to print
  *
  * Return: Nothing.
  */
@@ -12,15 +12,15 @@ void simple_print_buffer(char *buffer, unsigned int size)
 {
 unsigned int i;
 
-    i = 0;
-        while (i < size)
+i = 0;
+while (i < size)
 {
-		if (i % 10)
-		{
+	if (i % 10)
+	{
 		printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
+	}
+	if (!(i % 10) && i)
+	{
 		printf("\n");
 	}
 	printf("0x%02x", buffer[i]);
@@ -30,7 +30,7 @@ printf("\n");
 }
 
 /**
- * main -check the code
+ * main - check the code
  *
  * Return: Always 0.
  */
