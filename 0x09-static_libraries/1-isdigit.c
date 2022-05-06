4c433584082790d2ac5d138307a43c8f525d424f
Casley <Casleynkosi@gmail.com>
2022-05-06 14:19:53 +0000

    Task 0

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - check the code for Holberton School students
+ *
+ * @c: is an integer param
+ *
+ * Return: Always 0.
+ */
+
+
+int _isdigir(int c)
+{
+
+	return (c >= 48 && c <= 57);
+}
