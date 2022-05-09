3ab93e431d9015f7e1363f72dfa6ed217c9d5345
Casley <Casleynkosi@gmail.com>
2022-05-09 13:04:20 +0000

    Task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,12 +2,11 @@
 #include <stdio.h>
 
 /**
- * print_dog - prints a dog
- *
- * @d: dog to print 
- *
- * Return: void
+ * print_dog - prints a struct dog
+ * @d: pointer to struct type dog
+ * Return: No
  */
+
 void print_dog(struct dog *d)
 {
 	if (d == 0)
